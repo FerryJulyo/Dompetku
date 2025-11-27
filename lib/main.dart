0ff14d0f1d26e6aafda5c0672b2db69a8ea84529
FerryJulyo <ferryjulyo86@gmail.com>
2025-11-27 09:47:07 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,83 @@
+// main.dart (modified)
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
-import 'models/transaction.dart';
-import 'providers/finance_provider.dart';
-import 'pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'providers/wallet_provider.dart';
+import 'providers/txn_provider.dart';
+import 'providers/category_provider.dart';
+import 'providers/recurring_provider.dart';
+import 'providers/budget_provider.dart';
+import 'screens/home_screen.dart';
+import 'db/db_helper.dart';
+import 'utils/notify_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  Hive.registerAdapter(TransactionAdapter());
-  final box = await Hive.openBox('transactionsBox');
-  // ensure stored items are typed; if migrating from previous, ensure values are TransactionModel
-  runApp(MyApp(box));
+  await DBHelper.instance.init(); // inisialisasi DB (v3)
+  await NotifyService().init(); // inisialisasi notifikasi & timezone
+  runApp(const MoneyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final Box box;
-  const MyApp(this.box, {Key? key}) : super(key: key);
+class MoneyApp extends StatelessWidget {
+  const MoneyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => FinanceProvider(box),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => WalletProvider()),
+        ChangeNotifierProvider(create: (_) => TxnProvider()),
+        ChangeNotifierProvider(create: (_) => CategoryProvider()),
+        ChangeNotifierProvider(create: (_) => RecurringProvider()),
+        ChangeNotifierProvider(create: (_) => BudgetProvider()),
+      ],
       child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'SmartWallet',
+        title: 'MoneyApp',
         theme: ThemeData(
-          primarySwatch: Colors.indigo,
           useMaterial3: true,
+          textTheme: GoogleFonts.interTextTheme(),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        home: const HomePage(),
+        home: const StartupWrapper(),
       ),
     );
+  }
+}
+
+class StartupWrapper extends StatefulWidget {
+  const StartupWrapper({super.key});
+
+  @override
+  State<StartupWrapper> createState() => _StartupWrapperState();
+}
+
+class _StartupWrapperState extends State<StartupWrapper> {
+  bool _done = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _start();
+  }
+
+  Future<void> _start() async {
+    // process due recurrings on startup
+    final rp = Provider.of<RecurringProvider>(context, listen: false);
+    await rp.processDue();
+    // load other providers
+    await Provider.of<WalletProvider>(context, listen: false).load();
+    await Provider.of<TxnProvider>(context, listen: false).load();
+    await Provider.of<CategoryProvider>(context, listen: false).load();
+    await Provider.of<BudgetProvider>(context, listen: false).load();
+    setState(() => _done = true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_done) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+    return const HomeScreen();
   }
 }
